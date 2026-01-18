743f888e93dd8508a6470034c87c36227650d15c
soumayg9673 <soumayg9673@zohomail.in>
2026-01-18 06:06:42 +0000

    add sql file migrate

---
+++ data/migrate/18012026_0001.sql
@@ -0,0 +1,19 @@
+CREATE TABLE coupons (
+    id BIGSERIAL PRIMARY KEY,
+    code VARCHAR(90) NOT NULL UNIQUE,
+    amount INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE claims (
+    id SERIAL PRIMARY KEY,
+    coupon_id BIGINT NOT NULL,
+    user_id VARCHAR(90) NOT NULL,
+    claimed_at TIMESTAMP NOT NULL DEFAULT NOW(),
+
+    CONSTRAINT fk_coupon
+        FOREIGN KEY (coupon_id) REFERENCES coupons(id),
+
+    CONSTRAINT uq_coupon_user
+        UNIQUE (coupon_id, user_id)
+);
